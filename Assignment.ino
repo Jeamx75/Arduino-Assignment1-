db4ea33a6a53af8ffe0e3f76f376bfa390783170
Judah Ekow Affadu-Mensah <masterjupiter75@gmail.com>
2026-06-21 04:29:19 +0000

    Add files via upload
    Add final countdown sketch

---
+++ Assignment.ino
@@ -0,0 +1,37 @@
+int startValue = 5;
+byte ledPin = 13;
+ 
+void flashLED(int times){
+  while(times > 0){
+    digitalWrite(ledPin, 1);
+    delay(500);
+    digitalWrite(ledPin, 0);
+    delay(500);
+
+    times = times - 1;
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+
+  Serial.println(" ");
+  Serial.println("=== Smart Countdown Starting ===");
+
+  int count = startValue;
+  while(count > 0){
+    Serial.print("Count: ");
+    Serial.println(count);
+
+    flashLED(count);
+
+    delay(1000);
+    count = count - 1;
+  }
+  Serial.println("=== Countdown Complete ===");
+}
+ 
+void loop() {
+
+}
